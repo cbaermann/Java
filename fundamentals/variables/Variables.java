220313386d25f2e0b7bc9f1eb2055f05d7f13d8b
Cody Baermann <cbaermann@comcast.net>
2020-03-16 06:13:28 -0700

    variables

---
+++ fundamentals/variables/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        int ourInt; //declare variable
+        ourInt = 17;
+        double pi = 3.14159265;
+        boolean bool = true;
+        char singleChar = 'A';
+
+        String multipleChars = "ABC";
+    }
+}

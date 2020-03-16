837ec69c8638d21fac3c8f1e29afb8c9b0303d01
Cody Baermann <cbaermann@comcast.net>
2020-03-16 06:08:25 -0700

    playing with java

---
+++ fundamentals/javaFun/JavaFun.java
@@ -0,0 +1,7 @@
+public class JavaFun {
+    public static void main(String[] args) {
+        System.out.println("My name is Test");
+        System.out.println("I am 1000 years old");
+        System.out.println("Let's see if this works");
+    }
+}

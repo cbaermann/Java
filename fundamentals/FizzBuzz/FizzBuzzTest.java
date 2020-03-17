92a5d5cb507b09ed7084b80f18a17cea389683d8
Cody Baermann <cbaermann@comcast.net>
2020-03-17 06:54:40 -0700

    fizzbuzz:

---
+++ fundamentals/FizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,7 @@
+public class FizzBuzzTest{
+    public static void main(String[] args){
+        FizzBuzz test = new FizzBuzz();
+        String result = test.fizzBuzz(300);
+        System.out.println(result);
+    }
+}

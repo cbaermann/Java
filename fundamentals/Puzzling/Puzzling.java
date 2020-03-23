0e2cc614565060c2ff0db037a3acbc519a6816df
Cody Baermann <cbaermann@comcast.net>
2020-03-22 18:25:52 -0700

    shuffles names and also prints new array of strings with name greater than 5 char

--- fundamentals/Puzzling/Puzzling.java
+++ fundamentals/Puzzling/Puzzling.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Puzzling {
 
@@ -13,6 +14,20 @@ public class Puzzling {
         }
         System.out.println(sum);
         System.out.println(x);
+    }
+
+    public void shuffleNames(String[] arr){
+        ArrayList<String> shuffleNames = new ArrayList<String>();
+        ArrayList<String> greaterThan5 = new ArrayList<String>();
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i].length() > 5){
+                greaterThan5.add(arr[i]);
+            }
+            shuffleNames.add(arr[i]);
+        }
+        Collections.shuffle(shuffleNames);
+        System.out.println(greaterThan5);
+        System.out.println((shuffleNames));
     }
 
 }
--- fundamentals/Puzzling/PuzzlingTest.java
+++ fundamentals/Puzzling/PuzzlingTest.java
@@ -1,9 +1,12 @@
 public class PuzzlingTest {
     public static void main(String[] args) {
-        Puzzling testNumber = new Puzzling();
+        Puzzling testData = new Puzzling();
 
         int[] testArr = {3,5,1,2,7,9,8,13,25,32};
+        
+        String[] testNames = {"Nancy", "Jinichi", "Fujibayashi", "Momochi", "Ishiwaka"};
 
-        testNumber.sumAndGreaterThan10(testArr);
+        // testData.sumAndGreaterThan10(testArr);
+        testData.shuffleNames(testNames);
     }
 }

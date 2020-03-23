87f6259be3f7f759e31fcca96df960c462929301
Cody Baermann <cbaermann@comcast.net>
2020-03-22 19:14:45 -0700

    Used alternate method using set rather than map. notes added

--- fundamentals/HashMapFun/HashMapFun.java
+++ fundamentals/HashMapFun/HashMapFun.java
@@ -1,20 +1,38 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class HashMapFun {
     public static void main(String[] args) {
-        Map<String, String> userMap = new HashMap<String, String>();
 
+        //HashMap using Map rather than set, and using Map.Entry
+
+        
+        // Map<String, String> userMap = new HashMap<String, String>();
+
+        // userMap.put("nninja@codingdojo.com", "Nancy Ninja");
+        // userMap.put("ssamurai@codingdojo.com", "Sam Samurai");
+        // userMap.put("wwizard@codingdojo.com", "Walter Wizard");
+
+        // String name = userMap.get("nninja@codingdojo.com");
+        // System.out.println("The full name is: " + name);
+
+        // //get keys by using the keySet method
+        // for(Map.Entry<String, String> entry : userMap.entrySet()) {
+        //     System.out.println(entry.getKey() + " = " + entry.getValue());
+        // }
+
+
+        //HashMap using Set to iterate through
+        HashMap<String, String> userMap = new HashMap<String, String>();
         userMap.put("nninja@codingdojo.com", "Nancy Ninja");
         userMap.put("ssamurai@codingdojo.com", "Sam Samurai");
         userMap.put("wwizard@codingdojo.com", "Walter Wizard");
-
-        String name = userMap.get("nninja@codingdojo.com");
-        System.out.println("The full name is: " + name);
-
-        //get keys by using the keySet method
-        for(Map.Entry<String, String> entry : userMap.entrySet()) {
-            System.out.println(entry.getKey() + " = " + entry.getValue());
+        // get the keys by using the keySet method
+        Set<String> keys = userMap.keySet();
+        for(String key : keys) {
+            System.out.println(key);
+            System.out.println(userMap.get(key));    
         }
         }
     } 
